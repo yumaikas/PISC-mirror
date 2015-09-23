649edf79b6d947fd64482d18c5a2d80b266706ac
yumaikas94@gmail.com <yumaikas94@gmail.com>
2015-09-23 22:14:10 +0000

    Add word expansions, inspirations

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PISC
 Position Independent Source Code. A small, stack-based, concatenative language.
 
-This is currently a small side project to see how far a small stack-based scripting language can go in spare time. Inspired by Factor (80%), Forth (5%) and Python (15%). 
+This is currently a small side project to see how far a small stack-based scripting language can go in spare time. Inspired by Factor (80%), Forth (5%) and Python (5%) and Tcl (5%) (not 100% inspired yet.)
 Plans are currently for an interperated language.
 
 .pisc is the file extension. 
--- interpertor.go
+++ interpertor.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 	"unicode"
 	"unicode/utf8"
 )
 
 var (
-	EOF                  = fmt.Errorf("End of file")
-	EncodingFault        = fmt.Errorf("Encoding error!")
-	ConditionalTypeError = fmt.Errorf("Expected a boolean value, but didn't find it.")
+	EOF                       = fmt.Errorf("End of file")
+	EncodingFault             = fmt.Errorf("Encoding error!")
+	ConditionalTypeError      = fmt.Errorf("Expected a boolean value, but didn't find it.")
+	WordDefParenExpectedError = fmt.Errorf("Word definitions require a stack effect commnet!")
 )
 
 type word string
@@ -19,6 +21,11 @@ type word string
 type script struct {
 	idx  int
 	code string
+}
+
+type codeList struct {
+	idx  int
+	code []word
 }
 
 type codeSequence interface {
@@ -31,6 +38,8 @@ type machine struct {
 	values []stackEntry
 	// A map from words to slices of words.
 	definedWords map[word][]word
+	// TODO: try to work this out later.
+	definedStackComments map[word]string
 	// The top of the stack it the end of the []stackEntry slice.
 	// Every so many entries, we may need to re-allocate the stack....
 }
@@ -49,18 +58,25 @@ func (m *machine) popValue() stackEntry {
 	m.values = m.values[:len(m.values)-1]
 	return popped
 }
-
 func runCode(code string) {
-	intMatch := regexp.MustCompile(`\d+`)
-	// floatMatch := regexp.MustCompile(`\d+\.\d+`)
-
-	p := script{
+	p := &script{
 		idx:  0,
 		code: code,
 	}
+	m := machine{
+		values: make([]stackEntry, 0),
+	}
+	//
+	executeWordsOnMachine(m, p)
+}
 
-	m := machine{values: make([]stackEntry, 0)}
+var (
+	intMatch = regexp.MustCompile(`\d+`)
+	// floatMatch = regexp.MustCompile(`\d+\.\d+`)
+)
 
+// This executes a given code sequence against a given machine
+func executeWordsOnMachine(m machine, p codeSequence) {
 	var err error
 	var wordVal word
 	for err == nil {
@@ -99,7 +115,9 @@ func runCode(code string) {
 			// If there is an error, this will stop the loop.
 			err = m.runConditionalOperator()
 		case wordVal == "call":
-
+			// m.executeQuotation()
+		case wordVal == ":":
+			m.readWordDefinition(p)
 		default:
 			// Evaluate a defined word, or complain if a word is not defined.
 
@@ -107,10 +125,51 @@ func runCode(code string) {
 			// Hrm....
 		}
 		if err != nil {
-			fmt.Println("Execution stopped at index", p.idx, "Error: ", err)
+			// TODO: add some ways to debug here....
+			fmt.Println("Execution stopped Error: ", err)
 			return
 		}
 	}
+}
+
+func (m *machine) readWordDefinition(c codeSequence) error {
+	name, err := c.nextWord()
+	openPar, err2 := c.nextWord()
+	if openPar != "(" {
+		return WordDefParenExpectedError
+	}
+	// TODO: come back and clean this up.
+	if err != nil || err2 != nil {
+		return fmt.Errorf("Errors %s | %s", err.Error(), err2.Error())
+	}
+
+	stackComment := ""
+	// read the stack comment for the word
+	for err != nil {
+		wordVal, err := c.nextWord()
+		if err != nil {
+			return err
+		}
+		if wordVal == ")" {
+			break
+		}
+		stackComment += string(word) + " "
+	}
+	m.definedStackComments[name] = strings.Trim(stackComment)
+
+	wordDef := make([]word, 0)
+	for err != nil {
+		wordVal, err := c.nextWord()
+		if err != nil {
+			return err
+		}
+		if wordVal == ";" {
+			break
+		}
+		wordDef = append(wordDef, wordVal)
+	}
+	m.definedWords[name] = wordDef
+	return nil
 }
 
 func (m *machine) runConditionalOperator() error {
@@ -118,7 +177,7 @@ func (m *machine) runConditionalOperator() error {
 	trueVal := m.popValue()
 	booleanVal := m.popValue()
 
-	if b, ok := booleanval.(Boolean); ok {
+	if b, ok := booleanVal.(Boolean); ok {
 		if b {
 			m.pushValue(trueVal)
 		} else {
@@ -133,9 +192,19 @@ func (m *machine) runConditionalOperator() error {
 	}
 }
 
+func (c *codeList) nextWord() (word, error) {
+	if c.idx < len(c.code) {
+		retval := c.code[c.idx]
+		c.idx++
+		return retval, nil
+	}
+	return word(""), EOF
+}
+
 func (s *script) nextWord() (word, error) {
 	var readingInto string
 	for idx, r := range s.code[s.idx:] {
+		s.idx = idx
 		if r == utf8.RuneError {
 			return word(""), EncodingFault
 		}
