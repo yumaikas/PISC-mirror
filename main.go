001c2795d8aecb1ce4f4bd5a6d82f926f7ef5429
yumaikas94@gmail.com <yumaikas94@gmail.com>
2015-10-01 03:46:55 +0000

    Add basic, clunky REPL support to the main executable.

--- main.go
+++ main.go
@@ -4,6 +4,49 @@
 // Code
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// This function starts an interpertor
 func main() {
 	// Run command stuff here.
+	in := bufio.NewReader(os.Stdin)
+	m := &machine{
+		values:               make([]stackEntry, 0),
+		definedWords:         make(map[word][]word),
+		definedStackComments: make(map[word]string),
+	}
+
+	fmt.Print(
+		`Postion
+Independent
+Source
+Code
+`)
+	for {
+		fmt.Print(">> ")
+		line, err := in.ReadString('\n')
+		if line == "exit" {
+			fmt.Println("Exiting")
+			return
+		}
+		if err != nil {
+			panic(err)
+		}
+		words, spaces := getWordList(strings.TrimSpace(line))
+		p := &codeList{
+			idx:    0,
+			code:   words,
+			spaces: spaces,
+		}
+		executeWordsOnMachine(m, p)
+		fmt.Println("Data Stack:")
+		for _, val := range m.values {
+			fmt.Println(val.String())
+		}
+	}
 }
