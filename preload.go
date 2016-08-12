d8669ff91d6c20a55adde9a81d6d4dfa2e7d4055
yumaikas <yumaikas>
2016-08-12 07:51:56 +0000

    Trying to solve bathroom stalll problem

--- preload.go
+++ preload.go
@@ -19,6 +19,12 @@ func (m *machine) loadPredefinedValues() {
 	m.predefinedWords["f"] = NilWord(func(m *machine) {
 		m.pushValue(Boolean(false))
 	})
+	m.predefinedWords["dip"] = NilWord(func(m *machine) {
+		quot := m.popValue().(quotation)
+		a := m.popValue()
+		executeWordsOnMachine(m, &codeList{idx: 0, code: quot})
+		m.pushValue(a)
+	})
 	m.predefinedWords["pick-dup"] = NilWord(func(m *machine) {
 		distBack := int(m.popValue().(Integer))
 		m.pushValue(m.values[len(m.values)-distBack-1])
