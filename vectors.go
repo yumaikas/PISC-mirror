8ec7936a59220c95782557f1fd8c7e4e45eba527
yumaikas <yumaikas>
2016-08-12 06:14:48 +0000

    Adding a couple things

---
+++ vectors.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+)
+
+func (m *machine) loadVectorWords() error {
+
+	m.predefinedWords["vec-at"] = GoWord(func(m *machine) error {
+		// ( vec idx -- vec elem )
+		idx := int(m.popValue().(Integer))
+		arr := m.popValue().(Array)
+		if idx > len(arr)-1 || idx < 0 {
+			return fmt.Errorf("Index out of bounds!")
+		}
+		m.pushValue(arr[idx])
+		return nil
+	})
+
+	// ( -- vector )
+	m.predefinedWords["<vector>"] = NilWord(func(m *machine) {
+		m.pushValue(Array(make([]stackEntry, 0)))
+	})
+	// ( vec quot -- .. )
+	m.predefinedWords["vec-each"] = GoWord(func(m *machine) error {
+		quot := m.popValue().(quotation)
+		vect := m.popValue().(Array)
+		code := &codeList{
+			idx:  0,
+			code: quot,
+		}
+		for _, elem := range vect {
+			m.pushValue(elem)
+			code.idx = 0
+			err := executeWordsOnMachine(m, code)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	// ( vec elem -- newVect )
+	m.predefinedWords["vec-append"] = NilWord(func(m *machine) {
+		toAppend := m.popValue()
+		arr := m.popValue().(Array)
+		arr = append(arr, toAppend)
+		m.pushValue(arr)
+	})
+	// ( vec elem -- newVect )
+	m.predefinedWords["vec-prepend"] = NilWord(func(m *machine) {
+		toPrepend := m.popValue()
+		arr := m.popValue().(Array)
+		arr = append([]stackEntry{toPrepend}, arr...)
+		m.pushValue(arr)
+	})
+
+	// ( vec -- vec elem )
+	m.predefinedWords["vec-popback"] = GoWord(func(m *machine) error {
+		arr := m.popValue().(Array)
+		if len(arr) < 1 {
+			return fmt.Errorf("No elements in array!")
+		}
+		val := arr[len(arr)-1]
+		arr = arr[:len(arr)-1]
+		m.pushValue(arr)
+		m.pushValue(val)
+		return nil
+	})
+	// ( vec -- vec elem)
+	m.predefinedWords["vec-popfront"] = GoWord(func(m *machine) error {
+		arr := m.popValue().(Array)
+		if len(arr) < 1 {
+			return fmt.Errorf("No elements in array!")
+		}
+		val := arr[0]
+		arr = arr[1:]
+		m.pushValue(arr)
+		m.pushValue(val)
+		return nil
+	})
+	return nil
+}
