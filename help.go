e3368d5de0d3e41c0f3a609e0df8f00cbc9dfb11
yumaikas <yumaikas>
2016-09-17 07:19:14 +0000

    Made help a thing that I can work on

--- help.go
+++ help.go
@@ -1,16 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 // These are the help words
 
 func (m *machine) loadHelpWords() {
 	// ( val name -- )
 	m.predefinedWords["help"] = GoWord(func(m *machine) error {
-		searchTerm := m.popValue().(String)
-		if len(m.locals) <= 0 {
-			return ErrNoLocalsExist
+		searchTerm := m.popValue().(String).String()
+		if len(m.helpDocs) > 0 {
+			for k, v := range m.helpDocs {
+				if strings.Contains(string(k), searchTerm) || strings.Contains(string(v), searchTerm) {
+					fmt.Println("Word: ", k, "\n\tDescription: ", v)
+				}
+			}
+			return nil
+		} else {
+			fmt.Println("No help avaialble.")
+			return nil
 		}
-		m.locals[len(m.locals)-1][varName.String()] = m.popValue()
-		return nil
 	})
 
 }
