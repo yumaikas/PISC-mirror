c143922e26fffb5b09c470766b6e88a876d42ecd
yumaikas <yumaikas>
2017-02-20 05:27:06 +0000

    Adding metaquotation capabilities

---
+++ metaquotation.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (m *machine) loadQuotWords() error {
+	// This is probably a dangerous word...
+	m.addGoWord("quot-set-var", " ( quot key val -- ) ", GoWord(func(m *machine) error {
+		val := m.popValue()
+		key := m.popValue().String()
+		quot := m.popValue().(*quotation)
+		if !strings.HasPrefix(key, "_") {
+			return fmt.Errorf("attempted to set a private local (%v)", key)
+		}
+		if quot.locals == nil {
+			return fmt.Errorf("attempted to add local to word that has no locals")
+		}
+		quot.locals[key] = val
+		return nil
+	}))
+	m.addGoWord("quot-has-var", " ( quot key -- ? ) ", GoWord(func(m *machine) error {
+		key := m.popValue().String()
+		quot := m.popValue().(*quotation)
+		if !strings.HasPrefix(key, "_") {
+			return fmt.Errorf("attempted to get a private local (%v)", key)
+		}
+		if quot.locals == nil {
+			m.pushValue(Boolean(false))
+			return nil
+		}
+		_, found := quot.locals[key]
+		m.pushValue(Boolean(found))
+		return nil
+	}))
+	m.addGoWord("quot-get-var", " ( quot key -- val ) ", GoWord(func(m *machine) error {
+		key := m.popValue().String()
+		quot := m.popValue().(*quotation)
+		if !strings.HasPrefix(key, "_") {
+			return fmt.Errorf("attempted to get a private local (%v)", key)
+		}
+		if quot.locals == nil {
+			return fmt.Errorf("Attempted to get a local on a word that has no locals")
+		}
+		m.pushValue(quot.locals[key])
+		return nil
+	}))
+	return nil
+}
