bb3217d6a489900859ccad2c5746144ba69e9e54
yumaikas <yumaikas>
2017-03-21 22:01:24 +0000

    Noting that the code probably won't be stored in a sql image, at least for now

--- ImageSchema.sql
+++ ImageSchema.sql
@@ -1,5 +1,7 @@
--- This is the schemda for a PISC image
+-- TODO: I don't think I'm going to use this, at least for the known future
+-- TODO: BoltDB seems like a better fit for now.
 
+-- This was the schemda for a PISC image 
 
 Create Table vocabulary (
 	vocab_id INTEGER PRIMARY KEY,
@@ -28,6 +30,3 @@ Create Table word_documentation (
 Create Table kv_store (
 );
 
--------------------------------------------------
-Search: /\*([^*]+)\*/ : ([\w-]+) (\( [^)]+\)) [^;]+;
-Replace: :DOC \2 \3 \1 ;
