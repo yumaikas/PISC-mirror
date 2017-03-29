cac90d50a1f81b223f5b111e0a943c386499b906
yumaikas <yumaikas>
2017-03-29 15:16:27 +0000

    Add some notes as to what Go router to use

--- web-ide/main.go
+++ web-ide/main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+// To try out: https://github.com/pressly/chi
+
 func main() {
 	// Simple static webserver:
 	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./webroot"))))
