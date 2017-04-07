1a2180587ff37a6da2c4fcf65b0a13540f7514a3
yumaikas <yumaikas>
2017-04-07 01:00:17 +0000

    Investigating chi router

--- web-ide/main.go
+++ web-ide/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"github.com/pressly/chi"
+	"github.com/pressly/chi/middleware"
 	"log"
 	"net/http"
 )
@@ -9,5 +12,24 @@ import (
 
 func main() {
 	// Simple static webserver:
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./webroot"))))
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.FileServer("/", http.Dir("./webroot"))
+	r.Get("/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Testing now!")
+	})
+
+	r.Get("/test/:id/arrp/:foo", func(w http.ResponseWriter, r *http.Request) {
+		// FIXME: Not safe!!
+		fmt.Fprintln(w, chi.URLParam(r, "id"))
+		fmt.Fprintln(w, chi.URLParam(r, "foo"))
+	})
+	r.Get("/test/a", func(w http.ResponseWriter, r *http.Request) {
+		// FIXME: Not safe!!
+		fmt.Fprintln(w, "foo")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
