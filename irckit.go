3b98d1043d0769c04770f8308d6ed23d25647a72
The Boss <The Boss>
2017-04-16 08:35:10 +0000

    Add IRCKit stub

---
+++ irckit.go
@@ -0,0 +1,14 @@
+package main
+
+var ModIRCKit = PISCModule {
+    Author: "Andrew Owen",
+    Name: "IRCKit",
+    License: "MIT",
+    DocString: "A wrapper around IRCX, built to make it easy to write IRC bots",
+    Load: nil,
+}
+
+// TODO: Load IRCKit here, using IRCX libraryr
+
+
+
