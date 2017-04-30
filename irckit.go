c575b1e773bff3de7220058959955e946d2bedfc
yumaikas <yumaikas>
2017-04-30 04:50:01 +0000

    Formatting

--- irckit.go
+++ irckit.go
@@ -1,14 +1,11 @@
 package main
 
-var ModIRCKit = PISCModule {
-    Author: "Andrew Owen",
-    Name: "IRCKit",
-    License: "MIT",
-    DocString: "A wrapper around IRCX, built to make it easy to write IRC bots",
-    Load: nil,
+var ModIRCKit = PISCModule{
+	Author:    "Andrew Owen",
+	Name:      "IRCKit",
+	License:   "MIT",
+	DocString: "A wrapper around IRCX, built to make it easy to write IRC bots",
+	Load:      nil,
 }
 
-// TODO: Load IRCKit here, using IRCX libraryr
-
-
-
+// TODO: Load IRCKit here, using IRCX library
