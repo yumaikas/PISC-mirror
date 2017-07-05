2f37a41f7b86f9d2b4ae262b7fc7851b516feeff
yumaikas <yumaikas>
2017-07-05 07:20:36 +0000

    Refactored while and times to be a little more stack-trace friendly. As a side-effect, slightly faster

--- loops.go
+++ loops.go
@@ -8,43 +8,45 @@ var ModLoopCore = Module{
 	Load:      loadLoopCore,
 }
 
+func _doTimes(m *Machine) error {
+	toExec := m.PopValue().(*Quotation)
+	nOfTimes := m.PopValue().(Integer)
+	for i := int(0); i < int(nOfTimes); i++ {
+		m.PushValue(toExec)
+		err := m.ExecuteQuotation()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func _doWhile(m *Machine) error {
+	body := m.PopValue().(*Quotation).toCode()
+	pred := m.PopValue().(*Quotation).toCode()
+	for {
+		pred.Idx = 0
+		err := m.execute(pred)
+		if err != nil {
+			return err
+		}
+
+		if !bool(m.PopValue().(Boolean)) {
+			break
+		}
+		body.Idx = 0
+		err = m.execute(body)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func loadLoopCore(m *Machine) error {
-	m.PredefinedWords["times"] = GoWord(func(m *Machine) error {
-		toExec := m.PopValue().(*Quotation)
-		nOfTimes := m.PopValue().(Integer)
-		for i := int(0); i < int(nOfTimes); i++ {
-			m.PushValue(toExec)
-			err := m.ExecuteQuotation()
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	})
-
+	m.AddGoWord("times", "( times quot -- ... ) ", _doTimes)
 	// ( pred body -- .. )
-	m.PredefinedWords["while"] = GoWord(func(m *Machine) error {
-		body := m.PopValue().(*Quotation).toCode()
-		pred := m.PopValue().(*Quotation).toCode()
-
-		for {
-			pred.Idx = 0
-			err := m.execute(pred)
-			if err != nil {
-				return err
-			}
-
-			if !bool(m.PopValue().(Boolean)) {
-				break
-			}
-			body.Idx = 0
-			err = m.execute(body)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	})
+	m.AddGoWord("while", "( pred quot -- ... )", _doWhile)
 
 	return m.importPISCAsset("stdlib/loops.pisc")
 }
